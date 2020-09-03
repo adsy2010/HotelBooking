dd57bea1f09b623d088a02731984027282492745
Adam <adsy2010@gmail.com>
2020-09-03 16:41:43 +0100

    Added basic API CRUD methods

--- app/Http/Controllers/RoomTypeController.php
+++ app/Http/Controllers/RoomTypeController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\RoomType;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class RoomTypeController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse|Response
      */
     public function index()
     {
@@ -21,66 +23,87 @@ class RoomTypeController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
         //
+        return response();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse|Response
      */
     public function store(Request $request)
     {
         //
+        $roomType = RoomType::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'hasTV' => $request->hasTV,
+            'hasFacilities' => $request->hasFacilities
+        ]);
+        $roomType->save();
+
+        return response()->json(['status' => 'ok', 'data' => $roomType]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\RoomType  $roomType
-     * @return \Illuminate\Http\Response
+     * @param RoomType $roomType
+     * @return JsonResponse|Response
      */
     public function show(RoomType $roomType)
     {
         //
+        return response()->json(['status' => 'ok', 'data' => $roomType]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\RoomType  $roomType
-     * @return \Illuminate\Http\Response
+     * @param RoomType $roomType
+     * @return JsonResponse|Response
      */
     public function edit(RoomType $roomType)
     {
         //
+        return response()->json(['status'=>'ok','data' => $roomType]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\RoomType  $roomType
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param RoomType $roomType
+     * @return JsonResponse|Response
      */
     public function update(Request $request, RoomType $roomType)
     {
         //
+        $roomType->name = $request->name;
+        $roomType->description = $request->description;
+        $roomType->hasTV = $request->hasTV;
+        $roomType->hasFacilities = $request->hasFacilities;
+        $roomType->save();
+
+        return response()->json(['status' => 'ok', 'data' => $roomType]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\RoomType  $roomType
-     * @return \Illuminate\Http\Response
+     * @param RoomType $roomType
+     * @return JsonResponse|Response
      */
     public function destroy(RoomType $roomType)
     {
         //
+        $roomType->delete();
+        return response()->json(['status' => 'ok']);
     }
 }
