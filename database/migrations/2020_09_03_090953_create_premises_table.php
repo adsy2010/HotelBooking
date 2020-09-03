0c8190b6592406f4e0dd056670e63cf171294418
Adam <adsy2010@gmail.com>
2020-09-03 10:47:47 +0100

    Initial Commit

---
+++ database/migrations/2020_09_03_090953_create_premises_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePremisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('premises', function (Blueprint $table) {
+            $table->id();
+            $table->text('address');
+            $table->text('city');
+            $table->text('postcode');
+            $table->text('tel');
+            $table->text('email');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('premises');
+    }
+}
