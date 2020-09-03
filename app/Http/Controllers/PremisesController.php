b6bacd425990ece1d167a8bee281862991b57ecd
Adam <adsy2010@gmail.com>
2020-09-03 16:27:08 +0100

    Added basic API CRUD functionality

--- app/Http/Controllers/PremisesController.php
+++ app/Http/Controllers/PremisesController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Premises;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PremisesController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse|Response
      */
     public function index()
     {
@@ -21,66 +23,91 @@ class PremisesController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
         //
+        return response();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse|Response
      */
     public function store(Request $request)
     {
         //
+        $premises = Premises::create([
+            'address' => $request->address,
+            'city' => $request->city,
+            'postcode' => $request->postcode,
+            'tel' => $request->tel,
+            'email' => $request->email,
+            'description' => $request->description,
+        ]);
+        $premises->save();
+
+        return response()->json(['status' => 'ok', 'data' => $premises]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Premises  $premises
-     * @return \Illuminate\Http\Response
+     * @param Premises $premises
+     * @return JsonResponse|Response
      */
     public function show(Premises $premises)
     {
         //
+        return response()->json(['status' => 'ok', 'data' => $premises]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Premises  $premises
-     * @return \Illuminate\Http\Response
+     * @param Premises $premises
+     * @return JsonResponse|Response
      */
     public function edit(Premises $premises)
     {
         //
+        return response()->json(['status'=>'ok','data' => $premises]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Premises  $premises
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Premises $premises
+     * @return JsonResponse|Response
      */
     public function update(Request $request, Premises $premises)
     {
         //
+        $premises->address = $request->address;
+        $premises->city = $request->city;
+        $premises->postcode = $request->postcode;
+        $premises->tel = $request->tel;
+        $premises->email = $request->email;
+        $premises->description = $request->description;
+        $premises->save();
+
+        return response()->json(['status' => 'ok', 'data' => $premises]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Premises  $premises
-     * @return \Illuminate\Http\Response
+     * @param Premises $premises
+     * @return JsonResponse|Response
      */
     public function destroy(Premises $premises)
     {
         //
+        $premises->delete();
+        return response()->json(['status' => 'ok']);
     }
 }
