2563cbac5f146bb26cdc9ed2d7c1933af11d0692
Adam <adsy2010@gmail.com>
2020-09-03 22:52:36 +0100

    Added basic API CRUD functionality

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Room;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class RoomController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return JsonResponse|Response
      */
     public function index()
     {
@@ -21,66 +23,85 @@ class RoomController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create()
     {
         //
+        return response();
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return JsonResponse|Response
      */
     public function store(Request $request)
     {
         //
+        $room = Room::create([
+            'premise' => $request->premise,
+            'roomType' => $request->roomType,
+            'room' => $request->room,
+        ]);
+        $room->save();
+
+        return response()->json(['status' => 'ok', 'data' => $room]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
+     * @param Room $room
+     * @return JsonResponse|Response
      */
     public function show(Room $room)
     {
         //
+        return response()->json(['status' => 'ok', 'data' => $room]);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
+     * @param Room $room
+     * @return JsonResponse|Response
      */
     public function edit(Room $room)
     {
         //
+        return response()->json(['status'=>'ok','data' => $room]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Room $room
+     * @return JsonResponse|Response
      */
     public function update(Request $request, Room $room)
     {
         //
+        $room->premise = $request->premise;
+        $room->roomType = $request->roomType;
+        $room->room = $request->room;
+        $room->save();
+
+        return response()->json(['status' => 'ok', 'data' => $room]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Room  $room
-     * @return \Illuminate\Http\Response
+     * @param Room $room
+     * @return JsonResponse|Response
      */
     public function destroy(Room $room)
     {
         //
+        $room->delete();
+        return response()->json(['status' => 'ok']);
     }
 }
