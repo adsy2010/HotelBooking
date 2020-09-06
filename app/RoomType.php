671fba88deee20822e4e498e11e44154649db210
Adam <adsy2010@gmail.com>
2020-09-06 15:16:22 +0100

    Added fillable variable fields and method tags for find and findorfail

--- app/RoomType.php
+++ app/RoomType.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * @method static create(array $array)
+ * @method static find($roomtype)
+ * @method static findOrFail($roomtype)
  */
 class RoomType extends Model
 {
     //
+    protected $fillable = ['name', 'description', 'hasTV', 'hasFacilities'];
+
     public function rooms()
     {
         return $this->hasMany('App\Room', 'roomType', 'id');
